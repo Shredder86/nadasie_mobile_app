588ac15022b09ee727fc207cc3c257ad1b6ed07c
Shredder86 <50884358+Shredder86@users.noreply.github.com>
2025-08-20 07:05:17 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ const get = (k, fb = "") => (typeof process !== "undefined" && process.env?.[k])
 
 module.exports = {
   WC_API_URL: get("WC_API_URL", "https://nadasie.pl/wp-json/wc/v3"),
-  WC_CONSUMER_KEY: get("WC_CONSUMER_KEY", "ck_0bc5107b993e1a68d20da166ec06726c22d47399"),
-  WC_CONSUMER_SECRET: get("WC_CONSUMER_SECRET", "cs_ec00a0acf4da9993243cfd30b0f2d3eb344dbdc8")
+  WC_CONSUMER_KEY: get("WC_CONSUMER_KEY", ""),
+  WC_CONSUMER_SECRET: get("WC_CONSUMER_SECRET", "")
 };
