df8d51e774c5cadd74dd1613d5bfa365c388ecbb
Shredder86 <50884358+Shredder86@users.noreply.github.com>
2025-08-20 06:30:01 +0200

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,6 @@
+// src/theme.js
+export const primary = "#F28C28";   // pomarańcz
+export const secondary = "#2E7D32"; // zielony
+export const background = "#FFFFFF";
+export const text = "#1F2937";
+export const muted = "#F3F4F6";
